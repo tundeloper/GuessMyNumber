2ce20dcea0e2d965f31a24065ca5d27b871c74df
tundeloper <isiakababatunde123@gmail.com>
2021-07-07 23:31:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ document.querySelector('.check').addEventListener('click', function () {
     displayMessage('🍕 Correct Number!');
     document.querySelector('body').style.backgroundColor = '#60b347';
 
-    // document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').style.width = '15rem';
 
     document.querySelector('.number').textContent = secretNumber;
 
